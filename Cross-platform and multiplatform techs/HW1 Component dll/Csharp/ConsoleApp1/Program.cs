5a7ee448ea9c89be75d1b81191db63baf5f84a6c
Maksym Shevchenko <50591739+Gurdel@users.noreply.github.com>
2021-10-25 23:46:22 +0300

    added laboratory work report

--- Cross-platform and multiplatform techs/HW1 Component dll/Csharp/ConsoleApp1/Program.cs
+++ Cross-platform and multiplatform techs/HW1 Component dll/Csharp/ConsoleApp1/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleApp1
             ClassLibrary1.Solver.Secant(0, 0.5, 0.0001, 3, new double[] { 2.0, 2.0, -9.0 });
 
             ClassLibrary1.Solver.Help();
-            ClassLibrary1.Solver.ProcessFiles("C:\\Users\\maksy\\input.txt", "C:\\Users\\maksy\\output.txt");
+            ClassLibrary1.Solver.ProcessFiles("C:\\Users\\maksy\\input.txt", "output.txt");
         }
     }
 }
