035a1b5dbe15bbd0d9ebd457c36722608420225c
Maksym Shevchenko <50591739+Gurdel@users.noreply.github.com>
2021-10-31 23:13:46 +0200

    added IS lab3 SHA256 Kupyna

---
+++ Information security/lab3 Hash functions SHA Kupyna/ConsoleForTests/Program.cs
@@ -0,0 +1,72 @@
+﻿using crypto_lab_3;
+using crypto_lab_3.Kupyna;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ConsoleForTests
+{
+    class Program
+    {
+        static public void FindCollision(IHashFunc hashFunc, int nonce, string proof, Random random)
+        {
+            byte[] inp = new byte[nonce];
+            string hash;
+            //HashSet<string> foundHashes = new HashSet<string>();
+            ulong iterationsCounter = 0;
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            while (true)
+            {
+                iterationsCounter++;
+                RandomizeByteArr(inp, random);
+
+                hash = Encoding.ASCII.GetString(hashFunc.CalcHash(inp));
+                //foundHashes.Add(hash);
+
+                if (hash.StartsWith(proof))
+                    break;
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Found in {iterationsCounter} iterations \n Time spent {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        static public void RandomizeByteArr(byte[] inp, Random random)
+        {
+            for (int i = 0; i < inp.Length; ++i)
+                inp[i] = (byte)random.Next(255);
+        }
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            IHashFunc kupyna = new Kupyna();
+            IHashFunc sha256Func = new SHA256();
+
+            string proof = "";
+            int nonce = 55;
+
+            for (int i = 0; i < 5; i++)
+            {
+                proof += "0";
+                Console.WriteLine($"\t\tProof = {proof}");
+
+
+                Console.WriteLine("SHA256");
+                FindCollision(sha256Func, nonce, proof, random);
+                Console.WriteLine();
+
+                Console.WriteLine("Kupyna");
+                FindCollision(kupyna, nonce, proof, random);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Information security/lab3 Hash functions SHA Kupyna/crypto_lab_3/Kupyna/Kupyna.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crypto_lab_3.Kupyna
+{
+    public class Kupyna : IHashFunc
+    {
+        byte[] IV = new byte[64]
+        {
+            0x40, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0
+        };
+
+        public byte[] CalcHash(byte[] message)
+        {
+            byte[] paddedmessage = MessagePreprocessHelper.GetPaddedMessage512(message);
+
+            byte[][] blocks = MessagePreprocessHelper.SplitMessage512(paddedmessage);
+
+            byte[] resultBlockOperation = processBlockOperation(blocks);
+
+            return resultBlockOperation;
+        }
+
+
+        private byte[] processBlockOperation(byte[][] messageByBlocks)
+        {
+            byte[] h = IV;
+            for (int i = 0; i < messageByBlocks.Length; i++)
+            {
+                byte[] messageForTPlusCircle = XOR(messageByBlocks[i], h);
+
+                byte[] messageForTPlus = messageByBlocks[i];
+
+                byte[] messageAfterTPlusCircle = TPCircle.generateMessageAfterTPlusCircle(messageForTPlusCircle);
+
+                byte[] messageAfterTPlus = TPlus.generateMessageAfterTPlus(messageForTPlus);
+
+                h = lastBlockOperation(messageAfterTPlusCircle, messageAfterTPlus, h);
+            }
+
+            byte[] messageAfterFinalOperation = finalOperation(h);
+
+            byte[] cutMessage = cutTo256Bit(messageAfterFinalOperation);
+
+            return cutMessage;
+        }
+
+        private byte[] lastBlockOperation(byte[] messageAfterTPlusCircle, byte[] messageAfterTPlus, byte[] h_prev)
+        {
+            byte[] xorInputParameters = XOR(messageAfterTPlusCircle, messageAfterTPlus);
+
+            return XOR(xorInputParameters, h_prev);
+        }
+
+        private byte[] finalOperation(byte[] messageAfterLastBlockOperation)
+        {
+            byte[] messageForXOR = messageAfterLastBlockOperation;
+
+            byte[] messageAfterTPlusCircle = TPCircle.generateMessageAfterTPlusCircle(messageAfterLastBlockOperation);
+
+            return XOR(messageForXOR, messageAfterTPlusCircle);
+        }
+
+        private byte[] cutTo256Bit(byte[] messageAfterFinalOperation)
+        {
+            byte[] message256Bit = new byte[32];
+
+            for (int i = 0; i < 32; i++)
+            {
+                message256Bit[i] = messageAfterFinalOperation[32 + i];
+            }
+
+            return message256Bit;
+        }
+
+        public static byte[] XOR(byte[] firstArray, byte[] secondArray)
+        {
+            byte[] thirdArray = new byte[firstArray.Length];
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                thirdArray[i] = (byte)(firstArray[i] ^ secondArray[i]);
+            }
+            return thirdArray;
+        }
+    }
+}
